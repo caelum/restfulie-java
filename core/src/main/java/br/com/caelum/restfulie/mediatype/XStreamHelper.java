1e0c235c3ffab68e1f5895ad1d6fe2fad89ecdd9
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-06 04:01:56 -0300

    must ignore rule: basic

--- core/src/main/java/br/com/caelum/restfulie/mediatype/XStreamHelper.java
+++ core/src/main/java/br/com/caelum/restfulie/mediatype/XStreamHelper.java
@@ -5,13 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtField;
-import javassist.CtNewMethod;
-import javassist.NotFoundException;
-import br.com.caelum.restfulie.Resource;
 import br.com.caelum.restfulie.http.DefaultRelation;
 import br.com.caelum.restfulie.relation.Enhancer;
 
@@ -33,9 +26,9 @@ public class XStreamHelper {
 		}
 
 		public Object newInstance(Class originalType) {
-			if(realTypes.containsKey(originalType)) {
+			if (realTypes.containsKey(originalType)) {
 				return super.newInstance(realTypes.get(originalType));
-			} else if(!Modifier.isFinal(originalType.getModifiers())) {
+			} else if (!Modifier.isFinal(originalType.getModifiers())) {
 				// enhance now!
 				Class enhanced = new Enhancer().enhanceResource(originalType);
 				return super.newInstance(enhanced);
@@ -44,31 +37,45 @@ public class XStreamHelper {
 		}
 	}
 
-	private class LinkSupportWrapper extends MapperWrapper{
+	private class LinkSupportWrapper extends MapperWrapper {
 
 		public LinkSupportWrapper(Mapper wrapped) {
 			super(wrapped);
 		}
-		
+
+		@Override
+		public Class getItemTypeForItemFieldName(Class definedIn, String field) {
+			Class type = super.getItemTypeForItemFieldName(definedIn, field);
+			if (type == null) {
+				return String.class;
+			}
+			return type;
+		}
+
 		@Override
 		public String getFieldNameForItemTypeAndName(Class definedIn,
 				Class itemType, String itemFieldName) {
-			if(realTypes.containsKey(definedIn) && itemFieldName.equals("link")) {
+			if (itemFieldName.equals("id")) {
+				return "ha";
+			}
+			if (realTypes.containsKey(definedIn)
+					&& itemFieldName.equals("link")) {
 				return "link";
 			}
-			return super.getFieldNameForItemTypeAndName(definedIn, itemType, itemFieldName);
+			return super.getFieldNameForItemTypeAndName(definedIn, itemType,
+					itemFieldName);
 		}
-		
+
 		@Override
 		public Class realClass(String elementName) {
 			return super.realClass(elementName);
 		}
-	
+
 	}
 
 	private final HierarchicalStreamDriver driver;
 
-	private final Map<Class,Class> realTypes= new HashMap<Class,Class>();
+	private final Map<Class, Class> realTypes = new HashMap<Class, Class>();
 
 	public XStreamHelper(HierarchicalStreamDriver driver) {
 		this.driver = driver;
@@ -76,6 +83,7 @@ public class XStreamHelper {
 
 	/**
 	 * Extension point to define your own provider.
+	 * 
 	 * @return
 	 */
 	private ReflectionProvider getProvider() {
@@ -83,8 +91,9 @@ public class XStreamHelper {
 	}
 
 	/**
-	 * Extension point for configuring your xstream instance. 
-	 * @param typesToEnhance 
+	 * Extension point for configuring your xstream instance.
+	 * 
+	 * @param typesToEnhance
 	 * @return an xstream instance with support for link enhancement.
 	 */
 	public XStream getXStream(List<Class> typesToEnhance) {
@@ -99,14 +108,15 @@ public class XStreamHelper {
 		xstream.useAttributeFor(DefaultRelation.class, "rel");
 		xstream.useAttributeFor(DefaultRelation.class, "href");
 
-		for(Class type : typesToEnhance) {
+		for (Class type : typesToEnhance) {
 			realTypes.put(type, new Enhancer().enhanceResource(type));
 			xstream.processAnnotations(type);
 		}
-		for(Class customType : realTypes.values()) {
-			xstream.addImplicitCollection(customType, "link","link", DefaultRelation.class);
+		for (Class customType : realTypes.values()) {
+			xstream.addImplicitCollection(customType, "link", "link",
+					DefaultRelation.class);
 		}
 		return xstream;
 	}
-	
+
 }
