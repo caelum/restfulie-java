935ad0b3f3c964e3183402a8a3c319806f97c776
Guilherme Silveira & Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-08-19 14:48:16 -0300

    fixing part1 client

--- example/rest_from_scratch/part_1/client/src/main/java/br/com/caelum/client/TestGetRepresentation.java
+++ example/rest_from_scratch/part_1/client/src/main/java/br/com/caelum/client/TestGetRepresentation.java
@@ -1,18 +1,29 @@
 package br.com.caelum.client;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 import br.com.caelum.example.model.Item;
-import br.com.caelum.restfulie.Resources;
+import br.com.caelum.restfulie.Response;
+import br.com.caelum.restfulie.RestClient;
 import br.com.caelum.restfulie.Restfulie;
+import br.com.caelum.restfulie.mediatype.XmlMediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 public class TestGetRepresentation {
-	public static void main(String[] args) throws URISyntaxException {
-		
-		Resources server = Restfulie.resources();
-		server.configure(Item.class);
-		Item item = server.entryAt("http://localhost:3000/restfulie/items/2").accept("application/xml").get();
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		RestClient restfulie = Restfulie.custom();
+		restfulie.getMediaTypes().register(new XmlMediaType() {
+			@Override
+			protected void configure(XStream xstream) {
+				xstream.processAnnotations(Item.class);
+			}
+		});
+
+		Response response = restfulie.at("http://localhost:8080/restfulie/items/2").accept("application/xml").get();
+		Item item = response.getResource();
 		System.out.println(item.getName());
-		
+
 	}
 }
--- example/rest_from_scratch/part_1/client/src/main/java/br/com/caelum/client/TestPostRepresentation.java
+++ example/rest_from_scratch/part_1/client/src/main/java/br/com/caelum/client/TestPostRepresentation.java
@@ -1,18 +1,29 @@
 package br.com.caelum.client;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 import br.com.caelum.example.model.Item;
-import br.com.caelum.restfulie.Resources;
+import br.com.caelum.restfulie.Response;
+import br.com.caelum.restfulie.RestClient;
 import br.com.caelum.restfulie.Restfulie;
+import br.com.caelum.restfulie.mediatype.XmlMediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 public class TestPostRepresentation {
-	public static void main(String[] args) throws URISyntaxException {
-		Resources server = Restfulie.resources();
-		server.configure(Item.class);
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		RestClient restfulie = Restfulie.custom();
+		restfulie.getMediaTypes().register(new XmlMediaType() {
+			@Override
+			protected void configure(XStream xstream) {
+				xstream.processAnnotations(Item.class);
+			}
+		});
 
 		Item item = new Item("pipa", 299.0);
-		item = server.entryAt("http://localhost:3000/restfulie/items").as("application/json").post(item);
+		Response response = restfulie.at("http://localhost:8080/restfulie/items").as("application/xml").post(item);
+		item = response.getResource();
 		System.out.println(item.getName());
 	}
 }
