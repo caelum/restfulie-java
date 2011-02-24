72023e150ae3ebb2ee0f59c32ea1ac610f12ff4d
Jose Donizetti <jdbjunior@gmail.com>
2011-02-24 18:46:11 -0300

    all integration tests implemented.

--- client/src/test/java/br/com/caelum/restfulie/integration/client/ClientTest.java
+++ client/src/test/java/br/com/caelum/restfulie/integration/client/ClientTest.java
@@ -1,6 +1,9 @@
 package br.com.caelum.restfulie.integration.client;
 
+
 import static br.com.caelum.restfulie.Restfulie.resource;
+import static br.com.caelum.restfulie.opensearch.Url.page;
+import static br.com.caelum.restfulie.opensearch.Url.queryFor;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -8,7 +11,6 @@ import static org.hamcrest.Matchers.is;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.caelum.restfulie.Response;
@@ -88,99 +90,64 @@ public class ClientTest {
 		Order order = response.getResource();
 
 		response = resource(order).getLink("self").follow().handling("application/xml").put(orderParam);
-		response = pay(response);
+		order = response.getResource();
+		response = pay(order);
 
 		order = response.getResource();
 		assertThat(order.getState(), is(equalTo("processing_payment")));
 	}
 
-	private Response pay(Response response) {
-		Order order;
-		order = response.getResource();
-
+	private Response pay(Order order) {
 		Payment payment = new Payment("Guilherme Silveira", 444, order.getPrice());
-		response = resource(order).getLink("payment").follow().handling("application/xml").post(payment);
-		return response;
+		return resource(order).getLink("payment").follow().handling("application/xml").post(payment);
 	}
 
-	@Ignore
+	@Test
 	public void shouldTryAndPayForIt() {
-		Response response = search("20", 1);
+		Response response = search("20",1);
 		List<Product> products = response.getResource();
-
 		Product product = products.get(0);
-		Order orderParam = newOrder(product.getId());
-
-		response = resource(products).getLink("order").follow().handling("application/xml")
-				.post(newOrder("Av. Princesa Isabel 350, Copacabana, Rio de Janeiro"));
-
-		Order order = response.getResource();
-
-		response = resource(order).getLink("self").follow().handling("application/xml").put(orderParam);
-		response = pay(response);
-
+		
+		Order order = resource(products).getLink("order").follow().handling("application/xml").post(newOrder("Av. Princesa Isabel 350, Copacabana, Rio de Janeiro eh este")).getResource();
+		order = resource(order).getLink("self").follow().handling("application/xml").put(newOrder(product.getId())).getResource();
+		
+		response = pay(order);
+		
 		order = response.getResource();
-
-		response = waitPaymentSucesse(1, response);
-		order = response.getResource();
-
-		assertThat(order.getState(), is(equalTo("paid")));
-
+		
+		order = waitPaymentSuccess(1,order);
+		
+		assertThat(order.getState(),is(equalTo("preparing")));
 	}
 
-	// def wait_payment_success(attempts, result)
-	//
-	// results = search("20")
-	//
-	// results.resource.products.each do |product|
-	//
-	// result.order.state = w"paid"
-	// response =
-	// results.resource.products.links.order.follow.post(result.order)
-	//
-	// puts "Checking order status at #{result.order.links.self.href}"
-	// end
-	//
-	// if result.order.state == "unpaid" && attempts>0
-	// puts
-	// "Ugh! Payment rejected! Get some credits my boy... I am trying it again."
-	// result = pay(result)
-	// wait_payment_success(attempts-1, result)
-	// else
-	// result
-	// end
-	// end
-
-	private Response waitPaymentSucesse(int attempts, Response otherResponse) {
-		Response response = search("20", 1);
-
-		List<Product> products = response.getResource();
-
-		Order order = otherResponse.getResource();
-		for (Product product : products) {
-			order.setState("paid");
-
-			Order anotherOrder = response.getResource();
-			resource(anotherOrder.getProduct()).getLink("order").follow().post(order);
-
-			System.out.println("Checking order status at " + resource(order).getLink("self").getHref());
-
+	private Order waitPaymentSuccess(int attempts, Order order) {
+		
+		while(order.getState().equals("processing_payment")) {
+			
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("esperando");
+			order = resource(order).getLink("self").follow().handling("application/xml").get().getResource();
+			System.out.println(order.getState());
 		}
-
-		if (order.getState().equals("unpaid") && attempts > 0) {
-			System.out.println("Ugh! Payment reject! Get some credits my boy... I am trying it again.");
-			response = pay(otherResponse);
-			waitPaymentSucesse(attempts - 1, response);
+		
+		if(order.getState().equals("unpaid") && attempts > 0) {
+			System.out.println("payment rejected");
+			order = pay(order).getResource();
+			return waitPaymentSuccess(attempts - 1, order);
+		} else {
+			return order;
 		}
-
-		return response;
 	}
-
+	
 	private Response search(String term, int page) {
-		Response response = restfulie.at("http://localhost:3000/products/opensearch.xml")
-				.accept("application/opensearchdescription+xml").get();
+		Response response = restfulie.at("http://localhost:3000/products/opensearch.xml").accept("application/opensearchdescription+xml").get();
 		SearchDescription desc = response.getResource();
-		response = restfulie.at(desc.use("application/xml").atPage(page).search("20").getUri()).get();
+		response = desc.use("application/xml").with(queryFor(term)).and(page(page)).get();
 		return response;
 	}
 
