cae405493591744d5a55320e4610f28f80317eb0
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2011-05-18 12:48:28 -0300

    ignoring client test when not running serveer

--- client/src/test/java/br/com/caelum/restfulie/integration/client/ClientTest.java
+++ client/src/test/java/br/com/caelum/restfulie/integration/client/ClientTest.java
@@ -8,8 +8,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,6 +30,11 @@ public class ClientTest {
 
 	@Before
 	public void setUp() {
+		try {
+			new URL("http://localhost:3000").openStream();
+		} catch (IOException e) {
+			Assume.assumeNoException(e);
+		}
 		restfulie = Restfulie.custom();
 		XmlMediaType mediaType = new XmlMediaType().withTypes(SearchDescription.class, Url.class, Tags.class,
 				Product.class, Order.class, Item.class, Payment.class);
@@ -61,7 +69,7 @@ public class ClientTest {
 		Order orderParam = newOrder(product.get(0).getId());
 
 		Order order = response.getResource();
-		
+
 		System.out.println(resource(order).getLink("self").getType());
 		response = resource(order).getLink("self").follow().handling("application/xml").put(orderParam);
 
@@ -107,34 +115,34 @@ public class ClientTest {
 		Response response = search("20",1);
 		List<Product> products = response.getResource();
 		Product product = products.get(0);
-		
+
 		Order order = resource(products).getLink("order").follow().handling("application/xml").post(newOrder("Av. Princesa Isabel 350, Copacabana, Rio de Janeiro eh este")).getResource();
 		order = resource(order).getLink("self").follow().handling("application/xml").put(newOrder(product.getId())).getResource();
-		
+
 		response = pay(order);
-		
+
 		order = response.getResource();
-		
+
 		order = waitPaymentSuccess(1,order);
-		
+
 		assertThat(order.getState(),is(equalTo("preparing")));
 	}
 
 	private Order waitPaymentSuccess(int attempts, Order order) {
-		
+
 		while(order.getState().equals("processing_payment")) {
-			
+
 			try {
 				Thread.sleep(10000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			System.out.println("esperando");
 			order = resource(order).getLink("self").follow().handling("application/xml").get().getResource();
 			System.out.println(order.getState());
 		}
-		
+
 		if(order.getState().equals("unpaid") && attempts > 0) {
 			System.out.println("payment rejected");
 			order = pay(order).getResource();
@@ -143,7 +151,7 @@ public class ClientTest {
 			return order;
 		}
 	}
-	
+
 	private Response search(String term, int page) {
 		Response response = restfulie.at("http://localhost:3000/products/opensearch.xml").accept("application/opensearchdescription+xml").get();
 		SearchDescription desc = response.getResource();
