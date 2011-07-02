36abbb6f3d9fb0f54d83024267b64b4374e824e0
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-07-02 09:08:31 -0300

    javadoc

--- client/src/main/java/br/com/caelum/restfulie/request/RequestDispatcher.java
+++ client/src/main/java/br/com/caelum/restfulie/request/RequestDispatcher.java
@@ -5,6 +5,11 @@ import java.net.URI;
 import br.com.caelum.restfulie.Response;
 import br.com.caelum.restfulie.http.Request;
 
+/**
+ * A dispatcher that will actually execute the request.
+ * 
+ * @author Guilherme Silveira
+ */
 public interface RequestDispatcher {
 
 	Response process(Request request, String verb, URI uri, Object payload);
